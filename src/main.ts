afb7b017cc3bd93301ed12d9425c05680c52e5ca
LuciaAccenture <lucia.a.garcia@accenture.com>
2024-05-08 15:34:29 +0200

    [MOB-18] BE - Project Basic Configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 
-async function bootstrap() {
+async function main() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Esmorga API')
+    .setDescription('Swagger for Esmorga API.')
+    .setVersion('1.0')
+    .addTag('Events')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
-bootstrap();
+main();
