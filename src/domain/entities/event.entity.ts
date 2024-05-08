0e6005e800dbe3aae619d6342dbddf320125a2d8
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-05-08 17:24:44 +0200

    [MOB-19] Removing unnecessary decorators in entities

--- src/domain/entities/event.entity.ts
+++ src/domain/entities/event.entity.ts
@@ -1,5 +1,4 @@
 import {
-  IsNotEmpty,
   IsString,
   IsNumber,
   IsDateString,
@@ -10,36 +9,28 @@ import { Type } from 'class-transformer';
 import { Location } from './location.entity';
 
 export class Event {
-  @IsNotEmpty()
   @IsNumber()
   eventId: number;
 
-  @IsNotEmpty()
   @IsString()
   eventName: string;
 
-  @IsNotEmpty()
   @IsDateString()
   eventDate: Date;
 
-  @IsNotEmpty()
   @IsString()
   description: string;
 
-  @IsNotEmpty()
   @IsString()
   eventType: string;
 
-  @IsNotEmpty()
   @IsString()
   imageUrl: string;
 
-  @IsNotEmpty()
   @ValidateNested()
   @Type(() => Location)
   location: Location;
 
-  @IsNotEmpty()
   @IsArray()
   @IsString({ each: true })
   tags: string[];
