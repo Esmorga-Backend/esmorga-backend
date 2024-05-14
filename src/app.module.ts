96cc52a03b584e287ab42182857f82d493cb565a
LuciaAccenture <138872770+LuciaAccenture@users.noreply.github.com>
2024-05-14 12:23:49 +0200

    [MOB-18] BE - Project Basic Configs (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT),
+          },
+        }),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
