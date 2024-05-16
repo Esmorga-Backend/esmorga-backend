2cc40bfb00249b36e1bb4d5a443c73d4cf180a1b
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-05-16 13:25:27 +0200

    [MOB-19] improving event filter service

--- src/domain/services/filter-avaliable-events.ts
+++ src/domain/services/filter-avaliable-events.ts
@@ -3,13 +3,5 @@ import { Event } from '../entities';
 export function filterAvaliableEvents(eventList: Event[]): Event[] {
   const currentDate = new Date();
 
-  const avaliableEvents: Event[] = [];
-
-  eventList.map((event) => {
-    if (event.eventDate >= currentDate) {
-      avaliableEvents.push(event);
-    }
-  });
-
-  return avaliableEvents;
+  return eventList.filter((event) => event.eventDate >= currentDate);
 }
