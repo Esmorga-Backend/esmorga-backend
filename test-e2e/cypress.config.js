7103883c932d5c4b33f443f81982bd2db2865285
YagoAres <yago.ares.armesto@accenture.com>
2024-05-21 16:15:54 +0200

    Fixing and removing unnecesary files

--- test-e2e/cypress.config.js
+++ test-e2e/cypress.config.js
@@ -6,10 +6,9 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    
     },
-
-    // baseUrl: "http://localhost:3000", //Url base (por determinar)
-    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}' //Patrón de búsqueda de archivos de prueba
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}', //Patrón de búsqueda de archivos de prueba
+    supportFile	: false //Archivo de soporte no necesario
   },
 });
