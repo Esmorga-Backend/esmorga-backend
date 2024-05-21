9b8bee9946dbe3d175f6e95fbcee163ccbc6e2d5
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-05-21 13:22:40 +0200

    [MOB-25] Fixing env-var statements in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,28 +3,31 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { EventModule } from './infraestructure/http/modules';
 import { CacheModule } from '@nestjs/cache-manager';
 import { redisStore } from 'cache-manager-redis-yet';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { validateEnvVars } from './env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       validate: validateEnvVars,
     }),
     CacheModule.registerAsync({
       isGlobal: true,
-      useFactory: async () => ({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
         store: await redisStore({
           socket: {
-            host: process.env.REDIS_HOST,
-            port: parseInt(process.env.REDIS_PORT),
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
           },
         }),
       }),
     }),
     MongooseModule.forRootAsync({
-      useFactory: async () => ({
-        uri: process.env.MONGODB_URI,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
       }),
     }),
     EventModule,
