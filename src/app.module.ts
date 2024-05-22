94ed42614415e5eebe3ded1417e53c5943be9b3c
Jose Abrodos <120038500+JM-Abrodos@users.noreply.github.com>
2024-05-22 13:06:41 +0200

    [MOB-25] ENV Vars Validations (#11)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,33 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CacheModule } from '@nestjs/cache-manager';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { redisStore } from 'cache-manager-redis-yet';
+import { validateEnvVars } from './env.validation';
 import { EventModule } from './infraestructure/http/modules';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnvVars,
+    }),
     CacheModule.registerAsync({
       isGlobal: true,
-      useFactory: async () => ({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
         store: await redisStore({
           socket: {
-            host: process.env.REDIS_HOST,
-            port: parseInt(process.env.REDIS_PORT),
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
           },
         }),
       }),
     }),
     MongooseModule.forRootAsync({
-      useFactory: async () => ({
-        uri: process.env.MONGODB_URI,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
       }),
     }),
     EventModule,
---
+++ src/env.validation.ts
@@ -0,0 +1,46 @@
+import { plainToInstance } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  validateSync,
+  IsPositive,
+} from 'class-validator';
+
+class EnvVars {
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  APP_PORT: number;
+
+  @IsNotEmpty()
+  @IsString()
+  MONGODB_URI: string;
+
+  @IsNotEmpty()
+  @IsString()
+  REDIS_HOST: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  REDIS_PORT: number;
+}
+
+/**
+ * Validate if the configuration is according to specified conditions of EnvVars class
+ * @param config - An object containing the environment variables to be validated
+ * @returns EnvVars validated
+ * @throws {Error} - Throw and error if some envVar does not match the implemented decorators
+ */
+export function validateEnvVars(config) {
+  const validatedConfig = plainToInstance(EnvVars, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig);
+
+  if (errors.length) throw new Error(errors.toString());
+
+  return validatedConfig;
+}
