6886bf499b30c3994a29ec4535eaa1f2477e0fce
YagoAres <yago.ares.armesto@accenture.com>
2024-05-22 11:30:22 +0200

    Renaming file and adding baseUrl

--- test-e2e/cypress.config.js
+++ test-e2e/cypress.config.js
@@ -6,9 +6,11 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-    
+
     },
+
+    baseUrl: 'https://qa.esmorga.canarte.org/', //URL base de la aplicación
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}', //Patrón de búsqueda de archivos de prueba
-    supportFile	: false //Archivo de soporte no necesario
+    supportFile: false //Archivo de soporte no necesario
   },
 });
