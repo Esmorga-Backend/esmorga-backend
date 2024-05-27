4559a408efeb89feff0f9dcf57eaed9257600caa
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-05-27 12:34:12 +0200

    [MOB-35] Removing redis config and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { redisStore } from 'cache-manager-redis-yet';
 import { validateEnvVars } from './env.validation';
 import { EventModule } from './infraestructure/http/modules';
 
@@ -11,18 +9,6 @@ import { EventModule } from './infraestructure/http/modules';
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnvVars,
-    }),
-    CacheModule.registerAsync({
-      isGlobal: true,
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        store: await redisStore({
-          socket: {
-            host: configService.get('REDIS_HOST'),
-            port: configService.get('REDIS_PORT'),
-          },
-        }),
-      }),
     }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
