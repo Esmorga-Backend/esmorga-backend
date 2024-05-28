5e95bf53582dd1e02d456303512dec582168a68a
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-05-28 12:35:28 +0200

    [MOB-35] Improving logic that execute script migrations

--- src/config/execute-migrations.ts
+++ src/config/execute-migrations.ts
@@ -3,17 +3,24 @@ import { exec } from 'child_process';
 import * as util from 'util';
 
 export async function executeMigrations() {
-  const execPromise = util.promisify(exec);
+  try {
+    const execPromise = util.promisify(exec);
 
-  const scripts = {
-    PROD: 'npm run migrate:prod-up',
-    QA: 'npm run migrate:qa-up',
-    LOCAL: 'npm run migrate:local-up',
-  };
-  const scriptToExecute = scripts[process.env.NODE_ENV];
+    const scripts = {
+      PROD: 'npm run migrate:prod-up',
+      QA: 'npm run migrate:qa-up',
+      LOCAL: 'npm run migrate:local-up',
+    };
+    const scriptToExecute = scripts[process.env.NODE_ENV];
 
-  const { stdout, stderr } = await execPromise(scriptToExecute);
+    const { stderr } = await execPromise(scriptToExecute);
 
-  console.log(stdout);
-  console.error(stderr);
+    const stderrMigrationData = stderr
+      .split('\n')
+      .filter((line) => line.toLowerCase().includes('migration'));
+
+    console.log(stderrMigrationData);
+  } catch (error) {
+    console.error('Error executing DB migrations - ', error);
+  }
 }
