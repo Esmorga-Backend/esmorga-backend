c51d43ad0d4c02e217494e3d5afc8d3ba94e5454
Pablo Cañarte Sol <pablo.canarte.sol@accenture.com>
2024-05-28 12:57:33 +0200

    Fix Deploy Script

--- .github/workflows/scripts/DeployToEnv.py
+++ .github/workflows/scripts/DeployToEnv.py
@@ -5,7 +5,7 @@ pkey = paramiko.RSAKey.from_private_key(StringIO.StringIO(sys.argv[2]))
 print(hostname)
 ssh_client =paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey))
+ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
 command = "sudo systemctl stop esmorga.service;cd /opt/esmorga-backend/; git reset --hard HEAD ; git clean -df; git pull ;sudo systemctl start esmorga.service"
 stdin, stdout, stderr = ssh_client.exec_command(command)
 for line in stdout.readlines():
