0f7f7dceb8f330376404ed7b73485f97fae569ef
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-06-04 12:44:02 +0200

    [MOB-32] Adding pipe in main function to validate DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,35 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { executeMigrations } from './config';
 
 async function main() {
   executeMigrations();
+
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Esmorga API')
     .setDescription('Swagger for Esmorga API.')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
+
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(parseInt(process.env.APP_PORT));
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<number>('APP_PORT'));
 }
 
 main();
