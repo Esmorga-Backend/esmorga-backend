e17d18853b6dec7f65fda6dbb458912b178555f9
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-06-10 11:27:51 +0200

    [MOB-57] Pushing pending files

---
+++ src/infraestructure/db/services/validate-mapped-object.ts
@@ -0,0 +1,42 @@
+import { EventDTO } from '../../dtos';
+import { DataBaseInternalError } from '../errors';
+
+/**
+ * This funcition validates if the object mapped contain all required events fields,
+ * event if the required fied is nedted
+ * @param eventDto - Event following the expected format to be returned with db data
+ */
+export function validateEventDto(eventDto: EventDTO) {
+  const requiredEventFields = [
+    'eventId',
+    'eventName',
+    'eventDate',
+    'eventType',
+    'description',
+    'location.name',
+  ];
+
+  const missingFields = requiredEventFields.filter((field) => {
+    const fieldPath = field.split('.');
+    let currentObject = eventDto;
+
+    const isMissed = fieldPath.some((nestedField) => {
+      if (
+        !(nestedField in currentObject) ||
+        currentObject[nestedField] === undefined
+      ) {
+        return true;
+      }
+      currentObject = currentObject[nestedField];
+      return false;
+    });
+
+    return isMissed;
+  });
+
+  if (missingFields.length > 0) {
+    //TODO add a trace with the missingFields
+
+    throw new DataBaseInternalError();
+  }
+}
---
+++ test/jest-unit/infraestrcture/db/services/validated-mapped-object.spec.ts
@@ -0,0 +1,77 @@
+import { validateEventDto } from '../../../../../src/infraestructure/db/services';
+import { DataBaseInternalError } from '../../../../../src/infraestructure/db/errors';
+import { eventMock } from '../../../../mocks/dtos/event';
+
+describe('[unit-test] [validateEventDto]', () => {
+  it('Should throw an error if eventId is missed', () => {
+    try {
+      const event = { ...eventMock };
+
+      delete event.eventId;
+
+      validateEventDto(event);
+    } catch (error) {
+      expect(error).toBeInstanceOf(DataBaseInternalError);
+    }
+  });
+
+  it('Should throw an error if eventName is missed', () => {
+    try {
+      const event = { ...eventMock };
+
+      delete event.eventName;
+
+      validateEventDto(event);
+    } catch (error) {
+      expect(error).toBeInstanceOf(DataBaseInternalError);
+    }
+  });
+
+  it('Should throw an error if eventDate is missed', () => {
+    try {
+      const event = { ...eventMock };
+
+      delete event.eventDate;
+
+      validateEventDto(event);
+    } catch (error) {
+      expect(error).toBeInstanceOf(DataBaseInternalError);
+    }
+  });
+
+  it('Should throw an error if eventType is missed', () => {
+    try {
+      const event = { ...eventMock };
+
+      delete event.eventType;
+
+      validateEventDto(event);
+    } catch (error) {
+      expect(error).toBeInstanceOf(DataBaseInternalError);
+    }
+  });
+
+  it('Should throw an error if description is missed', () => {
+    try {
+      const event = { ...eventMock };
+
+      delete event.description;
+
+      validateEventDto(event);
+    } catch (error) {
+      expect(error).toBeInstanceOf(DataBaseInternalError);
+    }
+  });
+
+  it('Should throw an error if location.name is missed', () => {
+    try {
+      const event = JSON.parse(JSON.stringify(eventMock));
+
+      delete event.location.name;
+
+      validateEventDto(event);
+    } catch (error) {
+      expect(error).toBeInstanceOf(DataBaseInternalError);
+    }
+  });
+});
