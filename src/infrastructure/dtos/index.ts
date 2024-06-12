db87f12236d447e7cd5944feee39406b35876d76
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-06-12 13:18:45 +0200

    [MOB-32] Changing file name

--- src/infrastructure/dtos/index.ts
+++ src/infrastructure/dtos/index.ts
@@ -1,6 +1,6 @@
 export * from './event.dto';
 export * from './event-list.dto';
-export * from './accoount-login.dto';
+export * from './account-login.dto';
 export * from './user-profile-dto';
 export * from './account-logged.dto';
 export * from './pair-of-tokens';
