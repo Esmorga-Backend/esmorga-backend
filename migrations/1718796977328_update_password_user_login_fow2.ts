51e08be69dab83f73a614565d5d13c07f268aa7c
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-06-19 13:44:42 +0200

    [MOB-76] Fixing migration code

---
+++ migrations/1718796977328_update_password_user_login_fow2.ts
@@ -0,0 +1,57 @@
+import { Db } from 'mongodb';
+import { MigrationInterface } from 'mongo-migrate-ts';
+import { createHash } from 'crypto';
+
+function hash(string) {
+  return createHash('sha256').update(string).digest('hex');
+}
+
+const collection = 'users';
+
+const users = [
+  {
+    email: 'esmorga.test.01@yopmail.com',
+    password: 'Password01',
+    oldPassword: 'Password1',
+  },
+  {
+    email: 'esmorga.test.02@yopmail.com',
+    password: 'Password02',
+    oldPassword: 'Password2',
+  },
+  {
+    email: 'esmorga.test.03@yopmail.com',
+    password: 'Password03',
+    oldPassword: 'Password3',
+  },
+];
+
+export class update_password_user_login_fow1718793301030
+  implements MigrationInterface
+{
+  public async up(db: Db): Promise<any> {
+    const promises = users.map((user) => {
+      return db
+        .collection(collection)
+        .updateOne(
+          { email: { $eq: user.email } },
+          { $set: { password: hash(user.password) } },
+        );
+    });
+
+    await Promise.all(promises);
+  }
+
+  public async down(db: Db): Promise<any> {
+    const promises = users.map((user) => {
+      return db
+        .collection(collection)
+        .updateOne(
+          { email: { $eq: user.email } },
+          { $set: { password: hash(user.oldPassword) } },
+        );
+    });
+
+    await Promise.all(promises);
+  }
+}
