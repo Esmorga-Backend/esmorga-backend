5b2edef2ac810025b204f58a893c3e4faa9d13aa
Pablo Cañarte Sol <pablo.canarte.sol@accenture.com>
2024-06-19 17:21:05 +0200

    clean code format

--- test/jest-component/features/steps/events/get-events-steps.ts
+++ test/jest-component/features/steps/events/get-events-steps.ts
@@ -1,71 +1,72 @@
-import { app, eventRepository, schema, context } from '../../../steps-config'
-import { StepDefinitions} from 'jest-cucumber';
-import * as request from 'supertest';
 import { HttpStatus } from '@nestjs/common';
-import { futureEventMockDB, oldEventMockDB } from '../../../../mocks/db';
+import { StepDefinitions } from 'jest-cucumber';
 import { matchers } from 'jest-json-schema';
+import { eventRepository, context } from '../../../steps-config';
+import { futureEventMockDB, oldEventMockDB } from '../../../../mocks/db';
 
+export const getEventsSteps: StepDefinitions = ({ given, and, then }) => {
+  given('the GET Events API is available', () => {
+    context.path = '/v1/events';
+  });
 
+  given('the GET Events API is unavailable', () => {
+    context.path = '/v1/events';
+    jest.spyOn(eventRepository, 'find').mockRejectedValue(new Error());
+  });
 
-
-export const getEventsSteps: StepDefinitions = ({ given, and, when, then}) => {
-
-    given('the GET Events API is available', () => {
-      context.path = '/v1/events';
-    });
-
-    given('the GET Events API is unavailable', () => {
-      context.path = '/v1/events';
-      jest.spyOn(eventRepository, 'find').mockRejectedValue(new Error());
-    });
-
-    and(/^(\d+) Events in DB, (\d+) are in the past$/, (events_on_db,expired_events_on_db) => {
-      if (expired_events_on_db == 1 && events_on_db == 2 ){
-        jest.spyOn(eventRepository, 'find').mockResolvedValue([futureEventMockDB, oldEventMockDB]);
-      }else if (expired_events_on_db == 1 && events_on_db == 1 ){
-          jest.spyOn(eventRepository, 'find').mockResolvedValue([oldEventMockDB]);
-      }else if (events_on_db == 1){
-        jest.spyOn(eventRepository, 'find').mockResolvedValue([futureEventMockDB]);
-      }else if (expired_events_on_db == 0 && events_on_db == 0 ){
+  and(
+    /^(\d+) Events in DB, (\d+) are in the past$/,
+    (events_on_db, expired_events_on_db) => {
+      if (expired_events_on_db == 1 && events_on_db == 2) {
+        jest
+          .spyOn(eventRepository, 'find')
+          .mockResolvedValue([futureEventMockDB, oldEventMockDB]);
+      } else if (expired_events_on_db == 1 && events_on_db == 1) {
+        jest.spyOn(eventRepository, 'find').mockResolvedValue([oldEventMockDB]);
+      } else if (events_on_db == 1) {
+        jest
+          .spyOn(eventRepository, 'find')
+          .mockResolvedValue([futureEventMockDB]);
+      } else if (expired_events_on_db == 0 && events_on_db == 0) {
         jest.spyOn(eventRepository, 'find').mockResolvedValue([]);
-      }else{
-          expect(false).toBe(true)
+      } else {
+        expect(false).toBe(true);
       }
-    });
+    },
+  );
 
-  
-
-    then(/^the response should contain (\d+) upcoming Events$/, ( events_to_check) => {
+  then(
+    /^the response should contain (\d+) upcoming Events$/,
+    (events_to_check) => {
       expect(context.response.status).toBe(HttpStatus.OK);
       expect.extend(matchers);
-      expect(context.response.body).toMatchObject(
-        {"totalEvents": parseInt(events_to_check)}
-      )
-      if (events_to_check == 1){
-          expect(context.response.body).toEqual({
-            totalEvents: 1,
-            events: [
-              {
-                eventId: futureEventMockDB._id,
-                eventName: futureEventMockDB.eventName,
-                eventDate: futureEventMockDB.eventDate.toISOString(),
-                description: futureEventMockDB.description,
-                eventType: futureEventMockDB.eventType,
-                imageUrl: futureEventMockDB.imageUrl,
-                location: futureEventMockDB.location,
-                tags: futureEventMockDB.tags
-              },
-            ],
-          });
-      }else if (events_to_check == 0){
+      expect(context.response.body).toMatchObject({
+        totalEvents: parseInt(events_to_check),
+      });
+      if (events_to_check == 1) {
+        expect(context.response.body).toEqual({
+          totalEvents: 1,
+          events: [
+            {
+              eventId: futureEventMockDB._id,
+              eventName: futureEventMockDB.eventName,
+              eventDate: futureEventMockDB.eventDate.toISOString(),
+              description: futureEventMockDB.description,
+              eventType: futureEventMockDB.eventType,
+              imageUrl: futureEventMockDB.imageUrl,
+              location: futureEventMockDB.location,
+              tags: futureEventMockDB.tags,
+            },
+          ],
+        });
+      } else if (events_to_check == 0) {
         expect(context.response.body).toEqual({
           totalEvents: 0,
-          events: []
+          events: [],
         });
       } else {
-        expect(false).toBe(true)
-      }        
-    });
-    
-
-}
+        expect(false).toBe(true);
+      }
+    },
+  );
+};
--- test/jest-component/features/steps/events/post-events-steps.ts
+++ test/jest-component/features/steps/events/post-events-steps.ts
@@ -1,58 +1,63 @@
-import { app, eventRepository, schema, context } from '../../../steps-config'
-import { StepDefinitions} from 'jest-cucumber';
-import * as request from 'supertest';
 import { HttpStatus } from '@nestjs/common';
-import { futureEventMockDB, oldEventMockDB } from '../../../../mocks/db';
-import { matchers } from 'jest-json-schema';
+import { StepDefinitions } from 'jest-cucumber';
+import { eventRepository, context } from '../../../steps-config';
+import { createEventMock } from '../../../../mocks/dtos';
 
-import {  createEventMock,  createEventWithoutOptionalFieldsMock } from '../../../../mocks/dtos'
-import { isLatitude, isLongitude, isNotIn, isNumber } from 'class-validator';
-
-
-export const postEventsSteps: StepDefinitions = ({ given, and, when, then}) => {
+export const postEventsSteps: StepDefinitions = ({
+  given,
+  and,
+  then,
+}) => {
   given('the POST Events API is available', () => {
     context.path = '/v1/events';
   });
 
   and('an authenticated user with admin rights is logged in', () => {
-    console.log("To be developed -> an authenticated user with admin rights is logged in")
+    console.log(
+      'To be developed -> an authenticated user with admin rights is logged in',
+    );
   });
 
   and('with invalid data', () => {
     context.mock = { ...createEventMock };
-    
+
     delete context.mock.eventName;
   });
 
-
-
   and('should not be created', () => {
-    console.log("To be developed -> should not be created")
-
+    console.log('To be developed -> should not be created');
   });
 
-  and(/^with valid data, use tags: (.*), imageUrl: (.*), location.lat(.*) and location.long:(.*)$/, (tags, imageUrl, lat, long) => {
-    jest.spyOn(eventRepository, 'create').mockResolvedValue();
-    context.mock = { ...createEventMock };
+  and(
+    /^with valid data, use tags: (.*), imageUrl: (.*), location.lat(.*) and location.long:(.*)$/,
+    (tags, imageUrl, lat, long) => {
+      jest.spyOn(eventRepository, 'create').mockResolvedValue();
+      context.mock = { ...createEventMock };
 
-    context.mock.location.imageUrl=imageUrl;
-    context.mock.location.tags=tags;
-    if (lat!=""){context.mock.location.lat=parseInt(lat);}else{delete context.mock.location.lat}
-    if (long!=""){context.mock.location.long=parseInt(long);;}else{delete context.mock.location.long}
- 
-  });
+      context.mock.location.imageUrl = imageUrl;
+      context.mock.location.tags = tags;
+      if (lat != '') {
+        context.mock.location.lat = parseInt(lat);
+      } else {
+        delete context.mock.location.lat;
+      }
+      if (long != '') {
+        context.mock.location.long = parseInt(long);
+      } else {
+        delete context.mock.location.long;
+      }
+    },
+  );
   then(/^success response code (\d+) returned$/, (code_n) => {
-    if (code_n=201){
-      console.log(context.response)
+    if ((code_n = 201)) {
       expect(context.response.status).toBe(HttpStatus.CREATED);
       expect(context.response.body).toEqual({});
-    }else{
-      expect(false).toBe(true)
+    } else {
+      expect(false).toBe(true);
     }
   });
 
   and('should be created successfully', () => {
-    console.log("To be developed -> should be created successfully")
+    console.log('To be developed -> should be created successfully');
   });
-
-}
+};
