dcf43e33f7e7c3ddeb6bbdca4ce5d88ca4fdeacc
Jose Abrodos <120038500+JM-Abrodos@users.noreply.github.com>
2024-07-04 11:04:50 +0200

    [MOB-83] POST Login API - Email not case insensitive (#57)

--- src/infrastructure/http/dtos/account-login.dto.ts
+++ src/infrastructure/http/dtos/account-login.dto.ts
@@ -1,7 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class AccountLoginDto {
+  /**
+   * Transform is executed before @IsString() decorator so if value is not a string, @Transform()
+   * returns the request body value and breaks with @IsString() decorator logic
+   */
+  @Transform(({ value }) =>
+    typeof value === 'string' ? value.toLowerCase() : value,
+  )
   @IsString()
   @IsNotEmpty()
   @ApiProperty({ example: 'esmorga.test.01@yopmail.com' })
