6baa47c0497d59c9c3b428039b67475118d3d87e
Pablo Cañarte Sol <pablo.canarte.sol@accenture.com>
2024-07-09 17:36:18 +0200

    Test 12

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ export default defineConfig({
   test: {
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
-      reporter: ['text', 'json-summary', 'json'],
+      reporter: ['json-summary'],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
       reportOnFailure: true,
+    },
+    thresholds: {
+      lines: 60,
+      branches: 60,
+      functions: 60,
+      statements: 60,
     },
   },
 });
