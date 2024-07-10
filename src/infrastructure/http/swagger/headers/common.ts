5b2c107f33c1987386172fb31c88c2884e078e15
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-07-10 14:11:28 +0200

    [MOB-44] Updating swagger example for auth header

--- src/infrastructure/http/swagger/headers/common.ts
+++ src/infrastructure/http/swagger/headers/common.ts
@@ -9,6 +9,7 @@ export const AUTHORIZATION_BEARER = {
   required: true,
   schema: {
     type: 'string',
-    example: 'Bearer accessToken',
+    example:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiNjY4NDBmYjM3MDNlZTQxY2U2MGIzZGViIiwiaWF0IjoxNzIwMTY2ODMxLCJleHAiOjE3MjAxNjc0MzF9.Y8vpkTnsI5ry3hMbVVazeNQaZ0NDKqOQP3yEwJ1xhNu',
   },
 };
