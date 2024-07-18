5892da29c72677f611e7012df8379547a634b13b
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-07-18 10:52:42 +0200

    [MOB-44] Working on cucumber test MOB-TC-98

--- test/jest-component/features/steps/account/join-event-steps.ts
+++ test/jest-component/features/steps/account/join-event-steps.ts
@@ -8,12 +8,25 @@ import {
 } from '../../../../../src/infrastructure/db/repositories';
 import {
   FUTURE_EVENT_MOCK_DB,
+  OLD_EVENT_MOCK_DB,
   PAIR_OF_TOKENS_MOCK_DB,
 } from '../../../../mocks/db';
 
+const PATH = '/v1/account/events';
+
+const METHOD = 'post';
+
+const HEADERS = {
+  'Content-Type': 'application/json',
+  Authorization: 'Bearer accessToken',
+};
+
 export const joinEventSteps: StepDefinitions = ({ given, and }) => {
+  // ###### MOB-TC-53 ######
   given('I am authenticated', async () => {
-    context.path = '/v1/account/events';
+    context.path = PATH;
+
+    context.method = METHOD;
 
     context.jwtService = moduleFixture.get<JwtService>(JwtService);
 
@@ -37,16 +50,57 @@ export const joinEventSteps: StepDefinitions = ({ given, and }) => {
         context.eventParticipantsRepository,
         'findAndUpdateParticipantsList',
       )
-      .mockResolvedValue({});
+      .mockResolvedValue(null);
   });
 
   and('the accessToken is valid', () => {
-    jest.spyOn(context.jwtService, 'verifyAsync').mockResolvedValue(null);
+    jest.spyOn(context.jwtService, 'verifyAsync').mockResolvedValue({});
   });
 
   and('the eventId is valid', () => {
     jest
       .spyOn(context.eventRepository, 'findByIdentifier')
       .mockResolvedValue(FUTURE_EVENT_MOCK_DB);
+  });
+
+  // ###### MOB-TC-98 ######
+  given('I am authenticated with a valid accessToken', async () => {
+    context.path = PATH;
+
+    context.method = METHOD;
+
+    context.jwtService = moduleFixture.get<JwtService>(JwtService);
+
+    context.eventRepository =
+      moduleFixture.get<EventRepository>(EventRepository);
+
+    context.tokensRepository =
+      moduleFixture.get<TokensRepository>(TokensRepository);
+
+    context.eventParticipantsRepository =
+      moduleFixture.get<EventParticipantsRepository>(
+        EventParticipantsRepository,
+      );
+
+    jest.spyOn(context.jwtService, 'verifyAsync').mockResolvedValue({});
+
+    jest
+      .spyOn(context.tokensRepository, 'findOneByAccessToken')
+      .mockResolvedValue(PAIR_OF_TOKENS_MOCK_DB);
+
+    jest
+      .spyOn(
+        context.eventParticipantsRepository,
+        'findAndUpdateParticipantsList',
+      )
+      .mockResolvedValue(null);
+  });
+
+  and('i have provided a valid eventId', () => {});
+
+  and('the eventDate has already ended', () => {
+    jest
+      .spyOn(context.eventRepository, 'findByIdentifier')
+      .mockResolvedValue(OLD_EVENT_MOCK_DB);
   });
 };
