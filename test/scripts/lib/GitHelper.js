6bafd61b1af6e4b204a13c3aeb2130b047bc84c2
PabloCanarteSol <120370719+PabloCanarteSol@users.noreply.github.com>
2024-07-22 12:17:44 +0200

    [MOB-79] FIX - SSH rollback (#67)
    Co-authored-by: Pipe <devops@esmorga.canarte.org>

--- test/scripts/lib/GitHelper.js
+++ test/scripts/lib/GitHelper.js
@@ -1,24 +1,54 @@
 const { exec } = require('child_process');
 class GitHelper {
+  branchName = '';
+  constructor() {
+    this.seekBranchName();
+  }
+
   getBranchName() {
+    return this.branchName;
+  }
+  seekBranchName() {
     if ('GITHUB_HEAD_REF' in process.env) {
-      return process.env.GITHUB_HEAD_REF;
-    }
-    return new Promise((resolve, reject) => {
-      exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
-        if (err) {
-          reject(`Error executing git: ${err}`);
-          return;
-        }
-        if (stderr) {
-          reject(`Error in git: ${stderr}`);
-          return;
-        }
-        const branchName = stdout.trim();
-        resolve(branchName);
+      this.branchName = process.env.GITHUB_HEAD_REF;
+    } else {
+      this.branchName = new Promise((resolve, reject) => {
+        exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+          if (err) {
+            reject(`Error executing git: ${err}`);
+            return;
+          }
+          if (stderr) {
+            reject(`Error in git: ${stderr}`);
+            return;
+          }
+          const branchName = stdout.trim();
+          resolve(branchName);
+        });
       });
+    }
+    while (this.branchName == '') {
+      console.log('+++\n\n ' + this.branchName);
+    }
+  }
+  fixedBranchName() {
+    this.branchName = new Promise((resolve, reject) => {
+      exec(
+        'last_commit=$(git log -1 --format="%H"); branches=$(git branch -r --contains $last_commit); branch_name=$(echo "$branches" | grep -v "HEAD" | head -n 1 | sed "s|origin/||"); echo $branch_name',
+        (err, stdout, stderr) => {
+          if (err) {
+            reject(`Error executing git: ${err}`);
+            return;
+          }
+          if (stderr) {
+            reject(`Error in git: ${stderr}`);
+            return;
+          }
+          const branchName = stdout.trim();
+          resolve(branchName);
+        },
+      );
     });
   }
 }
-
 module.exports = GitHelper;
