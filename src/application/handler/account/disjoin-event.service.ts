63caaf00128727cc8acd015100618fd17480b8ac
Abrodos Torres, Jose Manuel <j.abrodos.torres@accenture.com>
2024-08-08 09:39:45 +0200

    [MOB-46] Creating command handler skeleton for disjoin event flow

---
+++ src/application/handler/account/disjoin-event.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
+import {
+  DataBaseUnathorizedError,
+  DataBaseNotFoundError,
+} from '../../../infrastructure/db/errors';
+import {
+  EventRepository,
+  TokensRepository,
+  EventParticipantsRepository,
+} from '../../../infrastructure/db/repositories';
+import {
+  BadEventIdApiError,
+  InvalidTokenApiError,
+  NotAccepteableEventApiError,
+} from '../../../domain/errors';
+
+@Injectable()
+export class DisJoinEventService {
+  constructor(
+    private readonly logger: PinoLogger,
+    private readonly eventRepository: EventRepository,
+    private readonly tokensRepository: TokensRepository,
+    private readonly eventParticipantsRepository: EventParticipantsRepository,
+  ) {}
+
+  async disJoinEvent(accessToken: string, eventId: string, requestId: string) {
+    try {
+      this.logger.info(
+        `[DisJoinEventService] [disJoinEvent] - x-request-id: ${requestId}, eventId ${eventId}`,
+      );
+
+      const { uuid } = await this.tokensRepository.getPairOfTokensByAccessToken(
+        accessToken,
+        requestId,
+      );
+
+      const { eventDate } = await this.eventRepository.getEvent(
+        eventId,
+        requestId,
+      );
+
+      if (eventDate < new Date()) throw new NotAccepteableEventApiError();
+    } catch (error) {
+      this.logger.error(
+        `[DisJoinEventService] [disJoinEvent] - x-request-id: ${requestId}, error ${error}`,
+      );
+
+      if (error instanceof DataBaseUnathorizedError)
+        throw new InvalidTokenApiError();
+
+      if (error instanceof DataBaseNotFoundError)
+        throw new BadEventIdApiError();
+
+      throw error;
+    }
+  }
+}
