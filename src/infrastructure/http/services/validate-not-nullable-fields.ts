aa5d5e7cd9961958f1c2871fb649afa30fb97872
LuciaAccenture <lucia.a.garcia@accenture.com>
2024-08-12 11:57:42 +0200

    [MOB-45] Complete a comment

--- src/infrastructure/http/services/validate-not-nullable-fields.ts
+++ src/infrastructure/http/services/validate-not-nullable-fields.ts
@@ -3,6 +3,7 @@ import { InvalidNullFieldApiError } from '../errors';
 /**
  * Validates if optional fields of the object are null.
  * @param fieldsToValidate Password saved in the DB for this user.
+ * @throws {Error} - Throw and error if any field that should not be `null` is found to be `null`.
  */
 export function validateNotNullableFields(fieldsToValidate: object) {
   Object.entries(fieldsToValidate).forEach(([key, value]) => {
