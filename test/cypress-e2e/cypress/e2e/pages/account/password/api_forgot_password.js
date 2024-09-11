3436ea9936514517961acaa7ad572911ed313f81
Pipe <devops@esmorga.canarte.org>
2024-09-11 16:48:04 +0200

    TEST

---
+++ test/cypress-e2e/cypress/e2e/pages/account/password/api_forgot_password.js
@@ -0,0 +1,33 @@
+import ApiBasics from '../../api_basic';
+import Api1Secmail from '../../../lib/api_1secmail';
+const api_1secmail = new Api1Secmail();
+class ApiForgotPassword extends ApiBasics {
+  #url = 'v1/account/password/forgot-init';
+  #email = 'auto.esmorga.test@1secmail.com';
+  constructor() {
+    super();
+  }
+
+  post() {
+    super.post(
+      this.#url,
+      {
+        email: this.#email,
+      },
+      {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    );
+  }
+  check_email() {
+    api_1secmail.get_emails();
+    cy.get('@response').then((response) => {
+      api_1secmail.get_email(response.body[0].id);
+      cy.get('@response').then((response) => {
+        cy.wrap(response.body).as('email');
+      });
+    });
+  }
+}
+export default ApiForgotPassword;
