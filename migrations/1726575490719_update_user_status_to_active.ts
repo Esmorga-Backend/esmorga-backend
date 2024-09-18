14323604dc7e06793abaf848b2d6ac25e9a95804
LuciaAccenture <lucia.a.garcia@accenture.com>
2024-09-18 11:18:32 +0200

    [MOB-132] Add migration

---
+++ migrations/1726575490719_update_user_status_to_active.ts
@@ -0,0 +1,27 @@
+import { Db } from 'mongodb';
+import { MigrationInterface } from 'mongo-migrate-ts';
+
+export class update_user_status_to_active1726575490719
+  implements MigrationInterface
+{
+  public async up(db: Db): Promise<any> {
+    await db.collection('users').updateMany(
+      {
+        $or: [{ status: { $ne: 'ACTIVE' } }, { status: { $exists: false } }],
+      },
+      { $set: { status: 'ACTIVE' } },
+    );
+  }
+
+  public async down(db: Db): Promise<any> {
+    await db.collection('users').updateMany(
+      {
+        $or: [
+          { status: { $ne: 'UNVERIFIED' } },
+          { status: { $exists: false } },
+        ],
+      },
+      { $set: { status: 'UNVERIFIED' } },
+    );
+  }
+}
