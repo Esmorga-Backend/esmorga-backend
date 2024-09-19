0449098e508a8019bff39b0d127c4b456e80f7c7
Pipe <devops@esmorga.canarte.org>
2024-09-19 11:16:28 +0200

    TEST 24

--- .github/workflows/scripts/createENV.py
+++ .github/workflows/scripts/createENV.py
@@ -7,13 +7,14 @@ failed_vars=[]
 for line in out:
     if line[:12]==' - property ':
         failed_vars.append(line[12:].split('has')[0][:-1])
-print(failed_vars)
 original=open('.github/actions/Deploy/original.yaml','r')
 formed=open('.github/actions/Deploy/action.yaml','w')
+values=''
 for line in original.readlines():
     if line.find('.github/workflows/scripts/DeployToEnv.py')!=-1:
         for var in failed_vars:
             value=var+"=\"${{secrets."+var+"}}${{vars."+var+"}}\""
             line=line+" "+value
-            print(value)
+            values=values+" "+value
     formed.write(line)
+print(values)
