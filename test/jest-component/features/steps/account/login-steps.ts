4b738f68834cd7bc5a5850930787340b703d0441
LuciaAccenture <lucia.a.garcia@accenture.com>
2024-09-24 13:38:28 +0200

    [MOB-132]

--- test/jest-component/features/steps/account/login-steps.ts
+++ test/jest-component/features/steps/account/login-steps.ts
@@ -1,24 +1,41 @@
 import { StepDefinitions } from 'jest-cucumber';
+import * as argon2 from 'argon2';
 import { getUserMockDb } from '../../../../mocks/db';
 import { context, moduleFixture } from '../../../steps-config';
-import { GenerateTokenPair } from '../../../../../src/domain/services';
+import {
+  GenerateTokenPair,
+  validateLoginCredentials,
+} from '../../../../../src/domain/services';
+import { ACCOUNT_STATUS } from '../../../../../src/domain/const';
+import { ACCOUNT_LOGIN_MOCK } from '../../../../mocks/dtos';
 
 import {
   AccountRepository,
   TokensRepository,
+  LoginAttemptsRepository,
 } from '../../../../../src/infrastructure/db/repositories';
 const TTL = parseInt(process.env.ACCESS_TOKEN_TTL);
 
-export const loginSteps: StepDefinitions = ({ given, and }) => {
+export const loginSteps: StepDefinitions = ({ given, and, then }) => {
   given('the POST Login API is available', async () => {
-    const USER_MOCK_DB = await getUserMockDb();
+    context.path = '/v1/account/login';
+
     context.accountRepository =
       moduleFixture.get<AccountRepository>(AccountRepository);
+
     context.tokensRepository =
       moduleFixture.get<TokensRepository>(TokensRepository);
+
+    // jest.spyOn(argon2, 'verify').mockResolvedValue(true);
+
+    // context.loginAttemptsRepository =
+    //   moduleFixture.get<LoginAttemptsRepository>(LoginAttemptsRepository);
+
     context.generateTokenPair =
       moduleFixture.get<GenerateTokenPair>(GenerateTokenPair);
-    context.path = '/v1/account/login';
+
+    const USER_MOCK_DB = await getUserMockDb();
+
     jest
       .spyOn(context.accountRepository, 'findOneByEmail')
       .mockResolvedValue(USER_MOCK_DB);
@@ -48,4 +65,38 @@ export const loginSteps: StepDefinitions = ({ given, and }) => {
       });
     },
   );
+
+  and('user status is UNVERIFIED', async () => {
+    const USER_MOCK_DB = await getUserMockDb();
+
+    const USER_MOCK_UNVERIFIED_DB = {
+      ...USER_MOCK_DB,
+      status: ACCOUNT_STATUS.UNVERIFIED,
+    };
+
+    jest
+      .spyOn(context.accountRepository, 'findOneByEmail')
+      .mockResolvedValue(USER_MOCK_UNVERIFIED_DB);
+  });
+
+  and('user status is BLOCKED', async () => {
+    const USER_MOCK_DB = await getUserMockDb();
+
+    const USER_MOCK_UNVERIFIED_DB = {
+      ...USER_MOCK_DB,
+      status: ACCOUNT_STATUS.BLOCKED,
+    };
+
+    jest
+      .spyOn(context.accountRepository, 'findOneByEmail')
+      .mockResolvedValue(USER_MOCK_UNVERIFIED_DB);
+  });
+
+  then(/^the result is that (.*)$/, async (result) => {
+    //   if (result === 'email error counter +1') {
+    //     expect(
+    //       context.loginAttemptsRepository.updateLoginAttempts,
+    //     ).toHaveBeenCalled();
+    //   }
+  });
 };
