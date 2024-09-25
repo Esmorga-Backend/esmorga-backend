2d44f45a127a2e988df6077bb4b4e24a1cf59fb3
Pipe <devops@esmorga.canarte.org>
2024-09-25 09:26:11 +0200

    TEST

--- .github/workflows/scripts/DeployToEnv.py
+++ .github/workflows/scripts/DeployToEnv.py
@@ -1,37 +1,45 @@
 import paramiko
 import sys
 import io
-key=sys.argv[4]
 service=sys.argv[2]
 service_path=sys.argv[3]
 hostname=sys.argv[1]
-pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
 print(hostname)
 ssh_client =paramiko.SSHClient()
 
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
+if len(sys.argv)>4:
+    key=sys.argv[4]
+    pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
+    ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
+else:
+    ssh_client.connect(hostname=hostname,port='22',username='ubuntu')
 command = """sudo systemctl stop """+service+"""
+sleep 30
 cd """+service_path+"""
-git reset --hard HEAD
-git clean -df
+#git reset --hard HEAD
+#git clean -df
+git checkout pipe/MOB-149
+git fetch
 git pull"""
+print(command)
 stdin, stdout, stderr = ssh_client.exec_command(command)
 for line in stdout.readlines():
     print(line)
 
 ftp_client=ssh_client.open_sftp()
 ftp_client.put('.env','.env')
-ftp_client.close()
 
 command = """
 cp .env """+service_path+"""
 cd """+service_path+"""
 npm ci
 sudo systemctl start  """+service
+print(command)
 stdin, stdout, stderr = ssh_client.exec_command(command)
 for line in stdout.readlines():
     print(line)
+ftp_client.close()
 ssh_client.close()
 
 
