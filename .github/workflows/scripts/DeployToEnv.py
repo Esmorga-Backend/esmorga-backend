4168a8ce68d4b6dd632bcedef54b71118e792842
Pipe <devops@esmorga.canarte.org>
2024-09-25 09:43:18 +0200

    ADD GITHUB_REF

--- .github/workflows/scripts/DeployToEnv.py
+++ .github/workflows/scripts/DeployToEnv.py
@@ -3,13 +3,14 @@ import sys
 import io
 service=sys.argv[2]
 service_path=sys.argv[3]
+branch=sys.argv[4]
 hostname=sys.argv[1]
 print(hostname)
 ssh_client =paramiko.SSHClient()
 
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-if len(sys.argv)>4:
-    key=sys.argv[4]
+if len(sys.argv)>5:
+    key=sys.argv[5]
     pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
     ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
 else:
@@ -17,11 +18,11 @@ else:
 command = """sudo systemctl stop """+service+"""
 sleep 30
 cd """+service_path+"""
-#git reset --hard HEAD
-#git clean -df
-git checkout pipe/MOB-149
-git fetch
-git pull"""
+sudo git reset --hard HEAD
+sudo git clean -df
+sudo git checkout """+branch+"""
+sudo git fetch
+sudo git pull"""
 print(command)
 stdin, stdout, stderr = ssh_client.exec_command(command)
 for line in stdout.readlines():
@@ -33,6 +34,7 @@ ftp_client.put('.env','.env')
 command = """
 cp .env """+service_path+"""
 cd """+service_path+"""
+chown ubuntu -R ./
 npm ci
 sudo systemctl start  """+service
 print(command)
