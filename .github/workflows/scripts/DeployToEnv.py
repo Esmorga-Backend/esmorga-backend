91dd18dfdc3c2e9dffcbaf3a5fcc0495b1d10e08
Pipe <devops@esmorga.canarte.org>
2024-09-25 08:12:50 +0200

    Starting Point

--- .github/workflows/scripts/DeployToEnv.py
+++ .github/workflows/scripts/DeployToEnv.py
@@ -1,7 +1,9 @@
 import paramiko
 import sys
 import io
-key=sys.argv[2]
+key=sys.argv[-1]
+service=sys.argv[2]
+service_path=sys.argv[3]
 hostname=sys.argv[1]
 pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
 print(hostname)
@@ -9,8 +11,8 @@ ssh_client =paramiko.SSHClient()
 
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
-command = """sudo systemctl stop esmorga.service
-cd /opt/esmorga-backend/
+command = """sudo systemctl stop """+service+"""
+cd """+service_path+"""
 git reset --hard HEAD
 git clean -df
 git pull"""
@@ -23,10 +25,10 @@ ftp_client.put('.env','.env')
 ftp_client.close()
 
 command = """
-cp .env /opt/esmorga-backend/
-cd /opt/esmorga-backend/
+cp .env """+service_path+"""
+cd """+service_path+"""
 npm ci
-sudo systemctl start esmorga.service"""
+sudo systemctl start  """+service
 stdin, stdout, stderr = ssh_client.exec_command(command)
 for line in stdout.readlines():
     print(line)
