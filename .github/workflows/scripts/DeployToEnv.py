cc385909156bcd5e6a54f591524f114dcef83ed2
PabloCanarteSol <120370719+PabloCanarteSol@users.noreply.github.com>
2024-09-25 10:27:31 +0200

    [MOB-149] Define BE PROD deployment strategy (#119)
    Co-authored-by: Pipe <devops@esmorga.canarte.org>
    Co-authored-by: EsmorgaPipeBot <pablo.canarte.accenture@gmail.com>

--- .github/workflows/scripts/DeployToEnv.py
+++ .github/workflows/scripts/DeployToEnv.py
@@ -1,35 +1,48 @@
 import paramiko
 import sys
 import io
-key=sys.argv[2]
+service=sys.argv[2]
+service_path=sys.argv[3]
+branch=sys.argv[4]
 hostname=sys.argv[1]
-pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
 print(hostname)
 ssh_client =paramiko.SSHClient()
 
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
-command = """sudo systemctl stop esmorga.service
-cd /opt/esmorga-backend/
-git reset --hard HEAD
-git clean -df
-git pull"""
+if len(sys.argv)>5:
+    key=sys.argv[5]
+    pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
+    ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
+else:
+    ssh_client.connect(hostname=hostname,port='22',username='ubuntu')
+command = """sudo systemctl stop """+service+"""
+sleep 30
+cd """+service_path+"""
+sudo git reset --hard HEAD
+sudo git clean -df"""
+if branch[:6]=="release":
+    command=command+"""sudo git checkout """+branch
+command=command+"""sudo git fetch
+sudo git pull"""
+print(command)
 stdin, stdout, stderr = ssh_client.exec_command(command)
 for line in stdout.readlines():
     print(line)
 
 ftp_client=ssh_client.open_sftp()
 ftp_client.put('.env','.env')
-ftp_client.close()
 
 command = """
-cp .env /opt/esmorga-backend/
-cd /opt/esmorga-backend/
+cp .env """+service_path+"""
+cd """+service_path+"""
+chown ubuntu -R ./
 npm ci
-sudo systemctl start esmorga.service"""
+sudo systemctl start  """+service
+print(command)
 stdin, stdout, stderr = ssh_client.exec_command(command)
 for line in stdout.readlines():
     print(line)
+ftp_client.close()
 ssh_client.close()
 
 
