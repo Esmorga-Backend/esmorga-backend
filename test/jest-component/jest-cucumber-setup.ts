04819721dcba8d59050e8a0435d49fe64449f270
Miguel Álvarez <miguel.alvarez.diez@accenture.com>
2024-10-15 16:15:58 +0200

    minor fix

--- test/jest-component/jest-cucumber-setup.ts
+++ test/jest-component/jest-cucumber-setup.ts
@@ -1,10 +1,7 @@
 import { loadFeatures, autoBindSteps } from 'jest-cucumber';
 import { directoryImport } from 'directory-import';
-if (process.env.DEBUG === 'jest') {
-  jest.setTimeout(5 * 60 * 1000);
-}
-const importedModules = directoryImport('./features/steps/');
 
+const importedModules = directoryImport('./features/steps/');
 const Steps: any = [];
 Object.values(importedModules).forEach((file) => {
   Object.values(file).forEach((exported) => {
