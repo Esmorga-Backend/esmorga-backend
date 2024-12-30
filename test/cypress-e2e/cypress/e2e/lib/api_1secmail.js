713289a6af8951eb271564be0b13c013c6907040
PabloCanarteSol <120370719+PabloCanarteSol@users.noreply.github.com>
2024-12-30 10:18:32 +0100

    Need to test on merge (#150)
    Co-authored-by: Pipe <devops@esmorga.canarte.org>
    Co-authored-by: Github Actions <actions@github.com>

--- test/cypress-e2e/cypress/e2e/lib/api_1secmail.js
+++ test/cypress-e2e/cypress/e2e/lib/api_1secmail.js
@@ -1,27 +1,64 @@
+function makeRequestWithRetries(url, options, retries = 3, delay = 15000) {
+  return new Cypress.Promise((resolve, reject) => {
+    cy.request({
+      url,
+      ...options,
+      failOnStatusCode: false,
+    }).then((response) => {
+      if (response.status === 500 && retries > 0) {
+        cy.wait(delay).then(() => {
+          makeRequestWithRetries(url, options, retries - 1, delay)
+            .then(resolve)
+            .catch(reject);
+        });
+      } else if (response.status === 500) {
+        reject(new Error('Server returned 500 after all retries'));
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
 class Api1Secmail {
   #login = 'auto.esmorga.test';
   #domain = '1secmail.com';
 
   get_emails() {
-    cy.request(
-      'GET',
+    makeRequestWithRetries(
       'https://www.1secmail.com/api/v1/?action=getMessages&login=' +
         this.#login +
         '&domain=' +
         this.#domain,
-    ).as('response');
+      { method: 'GET' },
+    )
+      .then((response) => {
+        cy.wrap(response).as('response');
+        expect(response.status).to.not.equal(500);
+      })
+      .catch((error) => {
+        cy.log(error.message);
+      });
+
     cy.wait(1000);
   }
   get_email(id) {
-    cy.request(
-      'GET',
+    makeRequestWithRetries(
       'https://www.1secmail.com/api/v1/?action=readMessage&login=' +
         this.#login +
         '&domain=' +
         this.#domain +
         '&id=' +
         id,
-    ).as('response');
+      { method: 'GET' },
+    )
+      .then((response) => {
+        cy.wrap(response).as('response');
+        expect(response.status).to.not.equal(500);
+      })
+      .catch((error) => {
+        cy.log(error.message);
+      });
+
     cy.wait(1000);
   }
 }
