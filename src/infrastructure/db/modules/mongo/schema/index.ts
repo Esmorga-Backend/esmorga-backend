471c3b220028e7dc17e3c145811856da26e29cf0
LuciaAccenture <lucia.a.garcia@accenture.com>
2025-09-17 11:39:19 +0200

    [MOB-354] Remove old tokens logic

--- src/infrastructure/db/modules/mongo/schema/index.ts
+++ src/infrastructure/db/modules/mongo/schema/index.ts
@@ -3,5 +3,4 @@ export * from './event-participants.schema';
 export * from './login-attempts.schema';
 export * from './session.schema';
 export * from './temporal-code.schema';
-export * from './tokens.schema';
 export * from './user.schema';
