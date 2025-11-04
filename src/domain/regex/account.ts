a1c205f8df77a2ba9834c667cfac776191b20cbd
LuciaAccenture <138872770+LuciaAccenture@users.noreply.github.com>
2025-11-04 15:24:16 +0100

    [MOB-468] BE – Change name and last name regex to accept Spanish characters (#205)
    Co-authored-by: Github Actions <actions@github.com>

--- src/domain/regex/account.ts
+++ src/domain/regex/account.ts
@@ -1,6 +1,6 @@
 // Regex for name and lastName is the same one
 export const ACCOUNT_REGEX = {
-  NAME: /^[A-Za-z\s'-]+$/,
+  NAME: /^[A-Za-zÁÉÍÓÚáéíóúÀÈÌÒÙàèìòùÜüÑñÇç\s'-]+$/,
   EMAIL: /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
   PASSWORD: /(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!-/:-@\[-`{-~])\S+$/,
 };
