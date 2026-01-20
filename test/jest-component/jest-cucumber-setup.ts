6466a4928ff64fb146e7b7a679c6aab0ff7d0ecf
LuciaAccenture <lucia.a.garcia@accenture.com>
2026-01-20 11:01:22 +0100

    More fixes

--- test/jest-component/jest-cucumber-setup.ts
+++ test/jest-component/jest-cucumber-setup.ts
@@ -1,13 +1,24 @@
 import { loadFeatures, autoBindSteps } from 'jest-cucumber';
 import { directoryImport } from 'directory-import';
+import * as glob from 'glob';
 
-const importedModules = directoryImport('./features/steps/');
-const Steps: any = [];
-Object.values(importedModules).forEach((file) => {
-  Object.values(file).forEach((exported) => {
-    Steps.push(exported);
+const featureFiles = glob.sync('test/jest-component/features/*.feature');
+
+if (featureFiles.length > 0) {
+  const importedModules = directoryImport('./features/steps/');
+  const Steps: any = [];
+  Object.values(importedModules).forEach((file) => {
+    Object.values(file).forEach((exported) => {
+      Steps.push(exported);
+    });
   });
-});
 
-const features = loadFeatures('test/jest-component/features/*.feature');
-autoBindSteps(features, Steps);
+  const features = loadFeatures('test/jest-component/features/*.feature');
+  autoBindSteps(features, Steps);
+} else {
+  describe('Component Tests', () => {
+    it('should skip when no Jira ticket is present', () => {
+      expect(true).toBe(true);
+    });
+  });
+}
