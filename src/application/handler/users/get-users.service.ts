36a2b90d5091779b2334132a0992e46eead4c162
HugoPanaAccenture <hugo.pena.cabarcos@accenture.com>
2026-01-30 09:34:41 +0100

    [ESM-49] Start get users service

--- src/application/handler/users/get-users.service.ts
+++ src/application/handler/users/get-users.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PinoLogger } from 'nestjs-pino';
+import { DataBaseUnauthorizedError } from '../../../infrastructure/db/errors';
+import { InvalidTokenApiError } from '../../../domain/errors';
+import { UserPaginatedItemDto } from '../../../infrastructure/dtos';
+
 @Injectable()
 export class GetUsersService {
-  constructor(private readonly logger: PinoLogger) {
+  constructor(private readonly logger: PinoLogger) {}
 
-  async getUsers(): Promise<void> {
+  async getUsers(requestId?: string): Promise<UserPaginatedItemDto[]> {
+    try {
+      this.logger.info(
+        `[GetUsersService] [getUsers] - x-request-id: ${requestId}`,
+      );
 
-      
+      return [];
+    } catch (error) {
+      this.logger.error(
+        `[GetUsersService] [getUsers] - x-request-id: ${requestId}, error: ${error}`,
+      );
+
+      if (error instanceof DataBaseUnauthorizedError)
+        throw new InvalidTokenApiError();
+
+      throw error;
     }
-
   }
 }
